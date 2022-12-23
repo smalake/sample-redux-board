0142b22288d169b841dfaa6ce0ec43542e79318e
smalake <koizumim1002@gmail.com>
2022-12-23 15:58:40 +0900

    Reduxを使ったサンプル掲示板の作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { addPost, deletePost } from "./features/Posts";
+import { useState } from "react";
 
 function App() {
+  // 引数のstateは現在の「state」の状態（Developer Toolで確認もできる）
+  const postList = useSelector((state) => state.posts.value);
+
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+
+  const dispacth = useDispatch();
+
+  const handleClick = () => {
+    dispacth(
+      addPost({
+        id: postList.length,
+        name: name,
+        content: content,
+      })
+    );
+    setName("");
+    setContent("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>React-Redux掲示板</h1>
+      </div>
+      <div className="addPost">
+        <input
+          type="text"
+          placeholder="お名前"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="text"
+          placeholder="投稿内容"
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+        />
+        <button onClick={() => handleClick()}>投稿</button>
+        <hr />
+      </div>
+      <div className="displayPosts">
+        {postList.map((post) => (
+          <div key={post.id} className="post">
+            <h2 className="postName">{post.name}</h2>
+            <h2 className="postContent">{post.content}</h2>
+            <button
+              onClick={() => {
+                dispacth(deletePost({ id: post.id }));
+              }}
+            >
+              削除
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
